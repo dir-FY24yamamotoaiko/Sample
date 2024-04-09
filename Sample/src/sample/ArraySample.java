e8eff46174118be6e715f956cf6616afe3171d6a
dir-FYyamamotoaiko <aiko.yamamoto.r095@dirbato.co.jp>
2024-04-09 19:23:30 +0900

    Java Lesson5 Chapter3

--- Sample/src/sample/ArraySample.java
+++ Sample/src/sample/ArraySample.java
@@ -9,6 +9,11 @@ public class ArraySample {
 		for (var i = 0; i < numbers.length; i++) {
 			numbers [i] = i * 2; //配列の各要素に値を代入
 		}
+		
+		//配列の要素を表示
+		for (var i = 0; i <numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
 
 	}
 
