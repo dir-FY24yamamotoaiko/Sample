61761a72baecb9154adf5a52e62c56a5be6c80f8
dir-FYyamamotoaiko <aiko.yamamoto.r095@dirbato.co.jp>
2024-04-09 17:36:05 +0900

    Java　Lesson5 Chapter1

---
+++ Sample/src/sample/ArraySample.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class ArraySample {
+
+	public static void main(String[] args) {
+		var numbers = new int[5];
+		
+		
+		for (var i = 0; i < numbers.length; i++) {
+			numbers [i] = i * 2; //配列の各要素に値を代入
+		}
+
+	}
+
+}
