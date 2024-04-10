b921de865fc417490d24b55bdbf396b2f9b61dc0
dir-FYyamamotoaiko <aiko.yamamoto.r095@dirbato.co.jp>
2024-04-10 12:44:27 +0900

    Java Lesson5Chapter4

---
+++ Sample/src/sample/StreamSample.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamSample {
+
+	public static void main(String[] args) {
+		List<String> places = new ArrayList<>();
+		
+		//要素の追加
+		places.add("アイスランド");
+		places.add("スペイン");
+		places.add("ナポリ");
+		places.add("リスボン");
+		places.add("ヨセミテ国立公園")
+		
+		//forEachを使った単純な出力
+		
+
+	}
+
+}
